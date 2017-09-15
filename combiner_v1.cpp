68e9888a72b43674ca6ab4c8bc79cc26994ef85c
torecan <torecancelik@gmail.com>
2017-09-15 13:35:27 +0200

    Without standarts
    Divider has been added but address are not standarts.

--- combiner_v1.cpp
+++ combiner_v1.cpp
@@ -16,6 +16,7 @@ using namespace cv;
 using namespace std;
 
 IplImage* capturer;
+IplImage* final_out;
 IplImage* img;
 IplImage* img1;
 IplImage* img2;
@@ -39,26 +40,56 @@ int nord_right[2] = { 1184,192 };
 
 int width, height;
 
+void A1Divider(IplImage*imge) {
+
+
+	cvSetImageROI(imge, cvRect(0, 0, a1_left[0], a1_left[1]));
+	cvSetImageROI(final_out, cvRect(0, 0, a1_left[0], a1_left[1]));
+
+	cvCopy(imge, final_out, NULL);
+
+	cvResetImageROI(final_out);
+	cvResetImageROI(imge);
+	cvSetImageROI(imge, cvRect(a1_left[0], 0, a1_right[0], a1_right[1]));
+	cvSetImageROI(final_out, cvRect(0, 192, a1_right[0], a1_right[1]));
+
+	cvCopy(imge, final_out, NULL);
+
+	cvResetImageROI(final_out);
+	cvResetImageROI(img);
+	
+	
+}
 
 
 
 void Combiner() {
-	
+
+	// RIGHT IMAGE
 
 	cvSetImageROI(capturer, cvRect(0, 0, img1->width, img1->height));
 	cvSetImageROI(img1, cvRect(0, 0, img1->width, img1->height));
-	
+
 	cvCopy(img1, capturer, NULL);
 	cvResetImageROI(img1);
 	cvResetImageROI(capturer);
 
-	// 
+	// VIDEO
 
-	cvSetImageROI(capturer, cvRect(img1->width, 0, resizer->width , resizer->height));
+	cvSetImageROI(capturer, cvRect(img1->width, 0, resizer->width, resizer->height));
 	cvSetImageROI(resizer, cvRect(0, 0, resizer->width, resizer->height));
 
-	cvCopy(resizer,capturer, NULL);
+	cvCopy(resizer, capturer, NULL);
 	cvResetImageROI(resizer);
+	cvResetImageROI(capturer);
+
+	//	LEFT IMAGE
+	
+	cvSetImageROI(capturer, cvRect((img1->width + resizer->width), 0, img2->width, img2->height));
+	cvSetImageROI(img2, cvRect(0, 0, img2->width, img2->height));
+
+	cvCopy(img2, capturer, NULL);
+	cvResetImageROI(img2);
 	cvResetImageROI(capturer);
 	
 }
@@ -67,39 +98,51 @@ void Combiner() {
 int main(int argc, char** argv) {
 
 	//cvNamedWindow("xample2", CV_WINDOW_AUTOSIZE);
-	CvCapture* capture = cvCreateFileCapture("C://Users//t.celik//Desktop//video.mp4");
-		
+	CvCapture* capture = cvCreateFileCapture("C://Users//t.celik//Desktop//A.A_Familienblock_01092017_Video.mp4");
+
 	img = cvQueryFrame(capture);
-	
+
 	img1 = cvLoadImage("C://Users//t.celik//Desktop//right.jpg");
 	img2 = cvLoadImage("C://Users//t.celik//Desktop//left.jpg");
+	final_out = cvCreateImage(cvSize(1920, 1080), img->depth, 3);
+
+
+
+	capturer = cvCreateImage(cvSize(2976, 192), img->depth, img->nChannels);
 	
-	
-	capturer= cvCreateImage(cvSize(5000, 192), img->depth, img->nChannels);
+	CvVideoWriter *writer = cvCreateVideoWriter(
+		"C://Users//t.celik//Desktop//write2r.avi",
+		CV_FOURCC('M', 'J', 'P', 'G'),
+		20,
+		cvSize(final_out->width, final_out->height));
 	
 
-	int r_wid = (2336 - 5 -(img1->width + img2->width));
-	
+	int r_wid = (2976 - (img1->width + img2->width));
+
 	resizer = cvCreateImage(cvSize(r_wid, 192), img->depth, img->nChannels);
 
-	cout << resizer->width << resizer->height;
+//	cout << resizer->width << resizer->height;
 
 	while (1) {
 
 		img = cvQueryFrame(capture);
-		cvResize(img,resizer);
-	
-		//cvShowImage("xample2", img);
+		if (!img) break;
+		
+		cvResize(img, resizer);
 		Combiner();
-		cvShowImage("xample3", capturer);
+		A1Divider(capturer);
+	//	char c = cvWaitKey(33);
+	//	if (c == 27) break;
+	//	cvShowImage("xample3", final_out);
+		
+		cvWriteFrame(writer, final_out);
 
-		char c = cvWaitKey(33);
-		if (c == 27) break;
 	}
 
+	cvReleaseImage(&img1);
+	cvReleaseImage(&img2);
+	cvReleaseVideoWriter(&writer);
 	cvReleaseCapture(&capture);
 	cvDestroyWindow("xample2");
 
 }
-
-
